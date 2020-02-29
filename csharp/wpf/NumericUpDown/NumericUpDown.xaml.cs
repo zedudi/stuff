9b78465b4ab4b0c95bca1824b8f854bc5ddf5e15
Dudás Zoltán <zoltan.dudas@basilisk.eu>
2020-02-29 12:43:44 +0100

    comments

--- csharp/wpf/NumericUpDown/NumericUpDown.xaml.cs
+++ csharp/wpf/NumericUpDown/NumericUpDown.xaml.cs
@@ -2,47 +2,76 @@
 using System.Windows;
 using System.Windows.Controls;
 
-namespace railcamsw.view
+namespace view.NumericUpDown
 {
+    /// <summary>
+    /// NumericUpDown is a gap-filling control for wpf applications.
+    /// Non-generic base class, as a workaround for wpf restictions, defining the callable properties from xaml.
+    /// </summary>
     public abstract partial class NumericUpDown : UserControl
     {
         abstract protected void Increase(object sender, RoutedEventArgs e);
         abstract protected void Decrease(object sender, RoutedEventArgs e);
-        public static DependencyProperty TextProperty;
     }
-    public partial class NumericUpDown <T> : NumericUpDown where T : struct
+    /// <summary>
+    /// Generic base class defining the operation of the controls.
+    /// </summary>
+    /// <typeparam name="T">Type of the content</typeparam>
+    public class NumericUpDown <T> : NumericUpDown where T : struct
     {
         System.Func<T, T> Inc, Dec;
 
-        public static new DependencyProperty TextProperty = DependencyProperty.Register(nameof(Text), typeof(T), typeof(NumericUpDown<T>), 
+        public static DependencyProperty TextProperty = DependencyProperty.Register(nameof(Text), typeof(T), typeof(NumericUpDown<T>), 
             new FrameworkPropertyMetadata(null) { BindsTwoWayByDefault = true });
+        /// <summary>
+        /// Constructor with lambdas for increase and decrease functions
+        /// </summary>
+        /// <param name="inc">lambda which invoked when the plus button pressed</param>
+        /// <param name="dec">lambda which invoked when the minus button pressed</param>
         public NumericUpDown(Func<T, T> inc, Func<T, T> dec)
         {
             InitializeComponent();
             Inc = inc;
             Dec = dec;
         }
-
+        /// <summary>
+        /// Visible property
+        /// </summary>
         public T Text
         {
             get { return (T)GetValue(TextProperty); }
             set { SetValue(TextProperty, value); }
         }
-        
-        override protected void Increase(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Increase button pressed event handler.
+        /// </summary>
+        /// <param name="_">Not used</param>
+        /// <param name="__">Not used</param>
+        override protected void Increase(object _, RoutedEventArgs __)
         {
             Text = Inc(Text);
         }
-        override protected void Decrease(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Increase button pressed event handler.
+        /// </summary>
+        /// <param name="_">Not used</param>
+        /// <param name="__">Not used</param>
+        override protected void Decrease(object _, RoutedEventArgs __)
         {
             Text = Dec(Text);
         }
     }
+    /// <summary>
+    /// Example subclass handling int.
+    /// </summary>
     public class NumericUpDownInt : NumericUpDown<int>
     {
         private const int unit = 1;
         public NumericUpDownInt() : base((x) => ( x + unit), (y)=> (y - unit)) { }
     }
+    /// <summary>
+    /// Example subclass handling float.
+    /// </summary>
     public class NumericUpDownFloat : NumericUpDown<float>
     {
         private static float unit = 0.1f;
--- csharp/wpf/NumericUpDown/README.md
+++ csharp/wpf/NumericUpDown/README.md
@@ -1,2 +1,2 @@
 #NumericUpDown
-Simple Spinner like wpf control, can be used as drop-in repacement for wpf TextBox, with bindable Text property.
+Simple Spinner like wpf control, can be used as drop-in repacement for wpf TextBox. The bindable text property has generic type, NumericUpDown generic class can be subclassed for adequate types.
